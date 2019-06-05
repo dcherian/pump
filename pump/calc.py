4ac38aa47cb32d8b9bb829bc684d8ea9d5230b85
dcherian <deepak@cherian.net>
2019-06-05 11:24:23 -0600

    Estimate approx N2 from T, S without estimating dens.

--- pump/calc.py
+++ pump/calc.py
@@ -11,12 +11,15 @@ def calc_reduced_shear(data):
     Estimate reduced shear for a dataset. Dataset must contain 'u', 'v', 'depth', 'dens'.
     '''
 
+    data['shear'] = np.hypot(data.u.differentiate('depth'),
+                             data.v.differentiate('depth'))
+    data['shear'].attrs['long_name'] = '|$u_z$|'
+    data['shear'].attrs['units'] = 's$^{-1}$'
 
-    data['shear'] = np.hypot(data.u.differentiate('depth'), data.v.differentiate('depth'))
-    data['dens'] = data.salt.copy(data=sw.pden(data.salt,
-                                               data.theta,
-                                               xr.broadcast(data.theta, data.depth)[1], 0))
-    data['N2'] = -9.81/1025 * data.dens.differentiate('depth')
+    data['N2'] = (9.81 * 1.7e-4 * data.theta.differentiate('depth')
+                  - 9.81 * 7.6e-4 * data.salt.differentiate('depth'))
+    data['N2'].attrs['long_name'] = '$N^2$'
+    data['N2'].attrs['units'] = 's$^{-2}$'
 
     data['shred2'] = data.shear**2 - 4 * data.N2
     data.shred2.attrs['long_name'] = 'Reduced shear$^2$'
