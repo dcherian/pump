36ca0eb2e89b90ec2315572f0132435911810589
dcherian <deepak@cherian.net>
2019-08-27 11:53:47 -0600

    black + plot_bulk_ri_diagnosis + TIW plot update.

--- pump/obs.py
+++ pump/obs.py
@@ -9,7 +9,7 @@ from . import mdjwf
 
 from .constants import *
 
-root = '/glade/p/nsc/ncgd0043/'
+root = "/glade/p/nsc/ncgd0043/"
 
 
 def read_all(domain=None):
@@ -21,71 +21,81 @@ def read_all(domain=None):
     return [johnson, tao, sst, oscar]
 
 
-def read_johnson(filename=root+'/obs/johnson-eq-pac-adcp.cdf'):
-    ds = (xr.open_dataset(filename)
-            .rename({'XLON': 'longitude',
-                     'YLAT11_101': 'latitude',
-                     'ZDEP1_50': 'depth',
-                     'POTEMPM': 'temp',
-                     'SALINITYM': 'salt',
-                     'SIGMAM': 'dens',
-                     'UM': 'u',
-                     'XLONedges': 'lon_edges'}))
+def read_johnson(filename=root + "/obs/johnson-eq-pac-adcp.cdf"):
+    ds = xr.open_dataset(filename).rename(
+        {
+            "XLON": "longitude",
+            "YLAT11_101": "latitude",
+            "ZDEP1_50": "depth",
+            "POTEMPM": "temp",
+            "SALINITYM": "salt",
+            "SIGMAM": "dens",
+            "UM": "u",
+            "XLONedges": "lon_edges",
+        }
+    )
 
-    ds['longitude'] -= 360
-    ds['depth'] *= -1
-    ds['depth'].attrs['units'] = 'm'
-    ds['u'].attrs['units'] = 'm/s'
+    ds["dens"] += 1000
+    ds["longitude"] -= 360
+    ds["depth"] *= -1
+    ds["depth"].attrs["units"] = "m"
+    ds["u"].attrs["units"] = "m/s"
 
     return ds
 
 
-def read_tao_adcp(domain=None, freq='dy'):
+def read_tao_adcp(domain=None, freq="dy"):
 
-    if freq == 'dy':
-        adcp = (xr.open_dataset(root+'/obs/tao/adcp_xyzt_dy.cdf')
-                .rename({'lon': 'longitude',
-                         'lat': 'latitude',
-                         'U_1205': 'u',
-                         'V_1206': 'v'}))
-    elif freq == 'hr':
-        afiles = [root+'/obs/tao/adcp0n'+lon+'_hr.cdf'
-                  for lon in ['156e', '165e', '170w', '140w', '110w']]
+    if freq == "dy":
+        adcp = xr.open_dataset(root + "/obs/tao/adcp_xyzt_dy.cdf").rename(
+            {"lon": "longitude", "lat": "latitude", "U_1205": "u", "V_1206": "v"}
+        )
+    elif freq == "hr":
+        afiles = [
+            root + "/obs/tao/adcp0n" + lon + "_hr.cdf"
+            for lon in ["156e", "165e", "170w", "140w", "110w"]
+        ]
 
         ds = []
         for file in tqdm.tqdm(afiles):
-            ds.append(xr.open_dataset(file)
-                      .drop(['QU_5205', 'QV_5206'])
-                      .rename({'lon': 'longitude', 'lat': 'latitude',
-                               'u_1205': 'u', 'v_1206': 'v'}))
-        adcp = xr.merge(xr.align(*ds, join='outer'))
+            ds.append(
+                xr.open_dataset(file)
+                .drop(["QU_5205", "QV_5206"])
+                .rename(
+                    {
+                        "lon": "longitude",
+                        "lat": "latitude",
+                        "u_1205": "u",
+                        "v_1206": "v",
+                    }
+                )
+            )
+        adcp = xr.merge(xr.align(*ds, join="outer"))
 
-    adcp = adcp.chunk({'latitude': 1, 'longitude': 1})
+    adcp = adcp.chunk({"latitude": 1, "longitude": 1})
 
     for vv in adcp:
         adcp[vv] /= 100
-        adcp[vv].attrs['long_name'] = vv
-        adcp[vv].attrs['units'] = 'm/s'
+        adcp[vv].attrs["long_name"] = vv
+        adcp[vv].attrs["units"] = "m/s"
         adcp[vv] = adcp[vv].where(np.abs(adcp[vv]) < 1000)
 
-    adcp['longitude'] -= 360
-    adcp['depth'] *= -1
-    adcp['depth'].attrs['units'] = 'm'
-    adcp['u'].attrs['units'] = 'm/s'
-    adcp['v'].attrs['units'] = 'm/s'
+    adcp["longitude"] -= 360
+    adcp["depth"] *= -1
+    adcp["depth"].attrs["units"] = "m"
+    adcp["u"].attrs["units"] = "m/s"
+    adcp["v"].attrs["units"] = "m/s"
 
     if domain is not None:
         adcp = adcp.sel(**domain)
     else:
         adcp = adcp.sel(latitude=0)
 
-    return (adcp
-            .dropna('longitude', how='all')
-            .dropna('depth', how='all'))
+    return adcp.dropna("longitude", how="all").dropna("depth", how="all")
 
 
 def read_eq_tao_temp_hr():
-    ''' Read hourly resolution temperature for equatorial moorings. '''
+    """ Read hourly resolution temperature for equatorial moorings. """
 
     # sfiles = [root+'/obs/tao/s0n'+lon+'_hr.cdf'
     #           for lon in ['156e', '165e', '140w', '110w', '170w']]
@@ -94,45 +104,52 @@ def read_eq_tao_temp_hr():
 
     def _read_and_merge(suffix):
         ds = []
-        tfiles = [f'{root}/obs/tao/t0n'+lon+'_'+suffix+'.cdf'
-                  for lon in ['156e', '165e', '170w', '140w', '110w']]
+        tfiles = [
+            f"{root}/obs/tao/t0n" + lon + "_" + suffix + ".cdf"
+            for lon in ["156e", "165e", "170w", "140w", "110w"]
+        ]
         for file in tqdm.tqdm(tfiles):
             try:
-                ds.append(xr.load_dataset(file)['T_20'])
+                ds.append(xr.load_dataset(file)["T_20"])
             except FileNotFoundError:
                 pass
 
-        merged = (xr.merge(xr.align(*ds, join='outer'))
-                  .rename({'lon': 'longitude', 'lat': 'latitude',
-                           'T_20': 'T'}))
+        merged = xr.merge(xr.align(*ds, join="outer")).rename(
+            {"lon": "longitude", "lat": "latitude", "T_20": "T"}
+        )
 
-        merged['longitude'] -= 360
-        merged['depth'] *= -1
+        merged["longitude"] -= 360
+        merged["depth"] *= -1
         return merged
 
-    m10 = _read_and_merge('10m')
-    hr = _read_and_merge('hr')
+    m10 = _read_and_merge("10m")
+    hr = _read_and_merge("hr")
 
     # resample is really slow because it doesn't know about dask.
     # instead reindex to a 10min freq and use rolling.
-    new_index = pd.date_range(start=m10.time[0].dt.round('H').values,
-                              end=m10.time[-1].dt.round('H').values,
-                              freq='10min')
+    new_index = pd.date_range(
+        start=m10.time[0].dt.round("H").values,
+        end=m10.time[-1].dt.round("H").values,
+        freq="10min",
+    )
     m10 = m10.reindex(time=new_index)
-    m10hr = (m10.chunk({'longitude': 1})
-             .rolling(time=6, min_periods=4)
-             .construct('window_dim', stride=6)
-             .mean('window_dim'))
+    m10hr = (
+        m10.chunk({"longitude": 1})
+        .rolling(time=6, min_periods=4)
+        .construct("window_dim", stride=6)
+        .mean("window_dim")
+    )
 
-    new_hourly_index = pd.date_range(start=np.min([m10.time[0].values,
-                                                   hr.time[0].values]),
-                                     end=np.max([m10.time[-1].values,
-                                                 hr.time[-1].values]),
-                                     freq='H')
+    new_hourly_index = pd.date_range(
+        start=np.min([m10.time[0].values, hr.time[0].values]),
+        end=np.max([m10.time[-1].values, hr.time[-1].values]),
+        freq="H",
+    )
 
-    concat = xr.concat([m10hr.reindex(time=new_hourly_index),
-                        hr.reindex(time=new_hourly_index)],
-                       dim='concat')
+    concat = xr.concat(
+        [m10hr.reindex(time=new_hourly_index), hr.reindex(time=new_hourly_index)],
+        dim="concat",
+    )
 
     # # T at 170W is only available at 10min frequency
     # ds.append(xr.load_dataset(root+'/obs/tao/t0n170w_10m.cdf')['T_20']
@@ -140,40 +157,55 @@ def read_eq_tao_temp_hr():
 
     # adcp = read_tao_adcp(freq='hr')
     # return xr.merge(ds)
-    return concat.mean('concat').squeeze().T
+    return concat.mean("concat").squeeze().T
 
 
 def read_tao(domain=None):
-    tao = (xr.open_mfdataset([root+'/obs/tao/'+ff
-                              for ff in ['t_xyzt_dy.cdf',
-                                         's_xyzt_dy.cdf',
-                                         'cur_xyzt_dy.cdf']],
-                             parallel=False,
-                             chunks={'lat': 1, 'lon': 1, 'depth': 5})
-           .rename({'U_320': 'u',
-                    'V_321': 'v',
-                    'T_20': 'temp',
-                    'S_41': 'salt',
-                    'lon': 'longitude',
-                    'lat': 'latitude'}))
-    tao['longitude'] -= 360
+    tao = xr.open_mfdataset(
+        [
+            root + "/obs/tao/" + ff
+            for ff in ["t_xyzt_dy.cdf", "s_xyzt_dy.cdf", "cur_xyzt_dy.cdf"]
+        ],
+        parallel=False,
+        chunks={"lat": 1, "lon": 1, "depth": 5},
+    ).rename(
+        {
+            "U_320": "u",
+            "V_321": "v",
+            "T_20": "temp",
+            "S_41": "salt",
+            "lon": "longitude",
+            "lat": "latitude",
+        }
+    )
+    tao["longitude"] -= 360
 
-    tao['u'] /= 100
-    tao['v'] /= 100
-    tao['u'].attrs['units'] = 'm/s'
-    tao['v'].attrs['units'] = 'm/s'
+    tao["u"] /= 100
+    tao["v"] /= 100
+    tao["u"].attrs["units"] = "m/s"
+    tao["v"].attrs["units"] = "m/s"
 
-    tao['depth'] *= -1
+    tao["depth"] *= -1
 
-    tao = tao.drop(['S_300', 'D_310', 'QS_5300', 'QD_5310',
-                    'QS_5041', 'SS_6041',
-                    'QT_5020', 'ST_6020', 'SRC_6300'])
+    tao = tao.drop(
+        [
+            "S_300",
+            "D_310",
+            "QS_5300",
+            "QD_5310",
+            "QS_5041",
+            "SS_6041",
+            "QT_5020",
+            "ST_6020",
+            "SRC_6300",
+        ]
+    )
 
     for vv in tao:
         tao[vv] = tao[vv].where(tao[vv] < 1e4)
-        tao[vv].attrs['long_name'] = ''
+        tao[vv].attrs["long_name"] = ""
 
-    tao['dens'] = mdjwf.dens(tao.salt, tao.temp, tao.depth)
+    tao["dens"] = mdjwf.dens(tao.salt, tao.temp, tao.depth)
 
     if domain is not None:
         return tao.sel(**domain)
@@ -184,25 +216,29 @@ def read_tao(domain=None):
 def read_sst(domain=None):
 
     if domain is not None:
-        years = range(pd.Timestamp(domain['time'].start).year,
-                      pd.Timestamp(domain['time'].stop).year)
+        years = range(
+            pd.Timestamp(domain["time"].start).year,
+            pd.Timestamp(domain["time"].stop).year,
+        )
         sst = xr.open_mfdataset(
-            [root+'/obs/oisst/sst.day.mean.'+str(yy)+'.nc' for yy in years],
-            parallel=True)
+            [root + "/obs/oisst/sst.day.mean." + str(yy) + ".nc" for yy in years],
+            parallel=True,
+        )
     else:
-        sst = xr.open_mfdataset(root+'/obs/oisst/sst.day.mean.*.nc',
-                                parallel=True)
+        sst = xr.open_mfdataset(root + "/obs/oisst/sst.day.mean.*.nc", parallel=True)
 
-    sst['lon'] -= 360
+    sst["lon"] -= 360
 
-    sst = sst.rename({'lat': 'latitude', 'lon': 'longitude'})
+    sst = sst.rename({"lat": "latitude", "lon": "longitude"})
 
-    sst['anom'] = sst.sst - sst.sst.mean(['longitude', 'time'])
-    sst['anom'].attrs['long_name'] = 'OISST Anomaly'
-    sst['anom'].attrs['units'] = r'$\degree$C'
-    sst['anom'].attrs['description'] = ('SST - mean(SST) in longitude, '
-                                        'time after subsetting to simulation '
-                                        'time length')
+    sst["anom"] = sst.sst - sst.sst.mean(["longitude", "time"])
+    sst["anom"].attrs["long_name"] = "OISST Anomaly"
+    sst["anom"].attrs["units"] = r"$\degree$C"
+    sst["anom"].attrs["description"] = (
+        "SST - mean(SST) in longitude, "
+        "time after subsetting to simulation "
+        "time length"
+    )
 
     if domain is not None:
         return sst.sel(**domain)
@@ -212,10 +248,11 @@ def read_sst(domain=None):
 
 def read_oscar(domain=None):
 
-    oscar = (dcpy.oceans.read_oscar(root+'/obs/oscar/')
-             .rename({'lat': 'latitude', 'lon': 'longitude'}))
-    oscar['longitude'] = oscar['longitude'] - 360
-    oscar = oscar.sortby('latitude')
+    oscar = dcpy.oceans.read_oscar(root + "/obs/oscar/").rename(
+        {"lat": "latitude", "lon": "longitude"}
+    )
+    oscar["longitude"] = oscar["longitude"] - 360
+    oscar = oscar.sortby("latitude")
 
     if domain is not None:
         return oscar.sel(**domain)
@@ -225,70 +262,120 @@ def read_oscar(domain=None):
 
 def read_argo():
 
-    dirname = root + '/obs/argo/'
-    chunks = {'LATITUDE': 1, 'LONGITUDE': 1}
+    dirname = root + "/obs/argo/"
+    chunks = {"LATITUDE": 1, "LONGITUDE": 1}
 
-    argoT = xr.open_dataset(dirname + 'RG_ArgoClim_Temperature_2017.nc',
-                            decode_times=False, chunks=chunks)
-    argoS = xr.open_dataset(dirname + 'RG_ArgoClim_Salinity_2017.nc',
-                            decode_times=False, chunks=chunks)
+    argoT = xr.open_dataset(
+        dirname + "RG_ArgoClim_Temperature_2017.nc", decode_times=False, chunks=chunks
+    )
+    argoS = xr.open_dataset(
+        dirname + "RG_ArgoClim_Salinity_2017.nc", decode_times=False, chunks=chunks
+    )
 
-    argoS['S'] = argoS.ARGO_SALINITY_ANOMALY + argoS.ARGO_SALINITY_MEAN
-    argoT['T'] = argoT.ARGO_TEMPERATURE_ANOMALY + argoT.ARGO_TEMPERATURE_MEAN
+    argoS["S"] = argoS.ARGO_SALINITY_ANOMALY + argoS.ARGO_SALINITY_MEAN
+    argoT["T"] = argoT.ARGO_TEMPERATURE_ANOMALY + argoT.ARGO_TEMPERATURE_MEAN
 
     argo = argoT.update(argoS)
 
-    argo = (argo.rename({'LATITUDE': 'latitude',
-                         'LONGITUDE': 'longitude',
-                         'PRESSURE': 'depth',
-                         'TIME': 'time',
-                         'ARGO_TEMPERATURE_MEAN': 'Tmean',
-                         'ARGO_TEMPERATURE_ANOMALY': 'Tanom',
-                         'ARGO_SALINITY_MEAN': 'Smean',
-                         'ARGO_SALINITY_ANOMALY': 'Sanom'}))
+    argo = argo.rename(
+        {
+            "LATITUDE": "latitude",
+            "LONGITUDE": "longitude",
+            "PRESSURE": "depth",
+            "TIME": "time",
+            "ARGO_TEMPERATURE_MEAN": "Tmean",
+            "ARGO_TEMPERATURE_ANOMALY": "Tanom",
+            "ARGO_SALINITY_MEAN": "Smean",
+            "ARGO_SALINITY_ANOMALY": "Sanom",
+        }
+    )
 
-    _, ref_date = xr.coding.times._unpack_netcdf_time_units(
-        argo.time.attrs['units'])
+    _, ref_date = xr.coding.times._unpack_netcdf_time_units(argo.time.attrs["units"])
 
-    argo.time.values = (pd.Timestamp(ref_date)
-                        + pd.to_timedelta(30 * argo.time.values, unit='D'))
-    argo['longitude'] -= 360
-    argo['depth'] *= -1
+    argo.time.values = pd.Timestamp(ref_date) + pd.to_timedelta(
+        30 * argo.time.values, unit="D"
+    )
+    argo["longitude"] -= 360
+    argo["depth"] *= -1
 
     return argo
 
 
 def process_nino34():
-    nino34 = process_esrl_index('nina34.data')
+    nino34 = process_esrl_index("nina34.data")
 
-    nino34.to_netcdf(root + '/obs/nino34.nc')
+    nino34.to_netcdf(root + "/obs/nino34.nc")
 
 
 def process_oni():
-    oni = process_esrl_index('oni.data', skipfooter=8)
-    oni.to_netcdf(root + '/obs/oni.nc')
+    oni = process_esrl_index("oni.data", skipfooter=8)
+    oni.to_netcdf(root + "/obs/oni.nc")
 
 
 def process_esrl_index(file, skipfooter=3):
-    ''' Read and make xarray version of climate indices from ESRL.'''
+    """ Read and make xarray version of climate indices from ESRL."""
 
-    month_names = (pd.date_range('01-Jan-2001', '31-Dec-2001', freq='MS')
-                   .to_series()
-                   .dt.strftime('%b').values
-                   .astype(str))
+    month_names = (
+        pd.date_range("01-Jan-2001", "31-Dec-2001", freq="MS")
+        .to_series()
+        .dt.strftime("%b")
+        .values.astype(str)
+    )
 
-    index = pd.read_csv(root+'/obs/'+file, index_col=0,
-                        names=month_names, delim_whitespace=True,
-                        skiprows=1, na_filter=False, skipfooter=skipfooter,
-                        dtype=np.float32)
+    index = pd.read_csv(
+        root + "/obs/" + file,
+        index_col=0,
+        names=month_names,
+        delim_whitespace=True,
+        skiprows=1,
+        na_filter=False,
+        skipfooter=skipfooter,
+        dtype=np.float32,
+    )
 
     flat = index.stack().reset_index()
-    flat['time'] = (pd.date_range('01-jan-'+str(flat['level_0'].iloc[0]),
-                                  '01-Jan-'+str(flat['level_0'].iloc[-1]+1),
-                                  freq='M'))
-    da = (flat.drop(['level_0', 'level_1'], axis=1)
-          .rename({0: 'index'}, axis=1)
-          .set_index('time')
-          .to_xarray())
+    flat["time"] = pd.date_range(
+        "01-jan-" + str(flat["level_0"].iloc[0]),
+        "01-Jan-" + str(flat["level_0"].iloc[-1] + 1),
+        freq="M",
+    )
+    da = (
+        flat.drop(["level_0", "level_1"], axis=1)
+        .rename({0: "index"}, axis=1)
+        .set_index("time")
+        .to_xarray()
+    )
 
-    return da.where(da > -90)['index']
+    return da.where(da > -90)["index"]
+
+
+def read_jra():
+    files = f"{root}/make_TPOS_MITgcm/JRA_FORCING/combined/JRA55DO_*_*wind.nc"
+
+    jra = xr.open_mfdataset(files, combine="by_coords", decode_times=False).rename(
+        {"lat": "latitude", "lon": "longitude"}
+    )
+
+    jra["time"] = jra.time - jra.time[0]
+    jra.time.attrs["units"] = "days since 1995-09-01"
+
+    jra["longitude"] -= 360
+
+    jra = jra.sel(longitude=slice(-170, -95))
+
+    return xr.decode_cf(jra)
+
+
+def read_drifters(kind="annual"):
+    if kind == "annual":
+        drifter = xr.load_dataset("~/pump/glade/obs/drifter/drifter_annualmeans.nc")
+
+    drifter = drifter.rename({"Lon": "longitude", "Lat": "latitude"}).roll(
+        longitude=720, roll_coords=True
+    )
+
+    drifter["longitude"] = (
+        xr.where(drifter.longitude < 0, drifter.longitude + 360, drifter.longitude)
+        - 360
+    )
+    return drifter.sel(longitude=slice(-230, -90))
--- pump/plot.py
+++ pump/plot.py
@@ -1,4 +1,7 @@
+import dcpy.plots
 import matplotlib.pyplot as plt
+import numpy as np
+import xarray as xr
 
 
 def plot_depths(ds, ax=None, **kwargs):
@@ -6,11 +9,142 @@ def plot_depths(ds, ax=None, **kwargs):
     if ax is None:
         ax = plt.gca()
 
-    if 'euc_max' in ds:
-        heuc = (ds.euc_max.plot.line(ax=ax, color='k', lw=1, _labels=False, **kwargs))
+    if "euc_max" in ds:
+        heuc = ds.euc_max.plot.line(ax=ax, color="k", lw=1, _labels=False, **kwargs)
 
-    if 'dcl_base' in ds:
-        hdcl = (ds.dcl_base.plot.line(ax=ax, color='gray', lw=1, _labels=False, **kwargs))
+    if "dcl_base" in ds:
+        hdcl = ds.dcl_base.plot.line(ax=ax, color="gray", lw=1, _labels=False, **kwargs)
 
-    if 'mld' in ds:
-        hmld = ((ds.mld).plot.line(ax=ax, color='k', lw=0.5, _labels=False, **kwargs))
+    if "mld" in ds:
+        hmld = (ds.mld).plot.line(ax=ax, color="k", lw=0.5, _labels=False, **kwargs)
+
+
+def plot_bulk_Ri_diagnosis(ds, f=None, ax=None, **kwargs):
+    """
+    Estimates fractional contributions of various terms to bulk Richardson
+    number.
+    """
+
+    def plot_ri_contrib(ax1, ax2, v, factor=1, **kwargs):
+        # Better to call differentiate on log-transformed variable
+        # This is a nicer estimate of the gradient and is analytically equal
+        per = factor * np.log(np.abs(v)).differentiate("longitude")
+        hdl = per.plot(
+            ax=ax2,
+            x="longitude",
+            label=f"{factor}/{v.name} $∂_x${v.name}",
+            add_legend=False,
+            **kwargs,
+        )
+        v.plot(ax=ax1, x="longitude", **kwargs)
+        ax1.set_xlabel("")
+        ax1.set_title("")
+
+        return per, hdl
+
+    if f is None and ax is None:
+        f, axx = plt.subplots(
+            7,
+            1,
+            constrained_layout=True,
+            sharex=True,
+            gridspec_kw={"height_ratios": [1, 1, 1, 1, 1, 1, 2]},
+        )
+        ax = dict(zip(["Ri", "h", "du", "db", "u", "b", "contrib"], axx))
+        add_legend = True
+    else:
+        add_legend = False
+
+    colors = dict(
+        {
+            "us": "C0",
+            "ueuc": "C1",
+            "bs": "C0",
+            "beuc": "C1",
+            "Ri": "C0",
+            "h": "C1",
+            "du": "C2",
+            "db": "C3",
+        }
+    )
+
+    factor = dict(zip(ax.keys(), [1, 1, -2, 1]))
+    rhs = xr.zeros_like(ds.bs)
+    per = dict()
+    for var in ax.keys():
+        if var not in ["u", "b", "contrib"]:
+            per[var], hdl = plot_ri_contrib(
+                ax[var],
+                ax["contrib"],
+                ds[var],
+                factor[var],
+                color=colors[var],
+                **kwargs,
+            )
+            if var != "Ri":
+                rhs += per[var]
+            else:
+                ri = per[var]
+                if "marker" not in kwargs:
+                    hdl[0].set_marker("o")
+
+    for vv in ["u", "b"]:
+        for vvar in ["s", "euc"]:
+            var = vv + vvar
+            if vvar == "euc":
+                factor = -1
+                prefix = "-"
+            else:
+                factor = 1
+                prefix = ""
+            (factor * ds[var].differentiate("longitude")).plot(
+                ax=ax[vv],
+                label=f"{prefix}$∂_x {vv}_{{{vvar}}}$",  # label=f'$∂_x{vv}_{{{vvar}}}$',
+                color=colors[var],
+                **kwargs,
+            )
+            if add_legend:
+                ax[vv].legend(ncol=2)
+
+            dcpy.plots.liney(0, ax[vv])
+            ax[vv].set_title("")
+            ax[vv].set_xlabel("")
+            ax[vv].set_ylabel("")
+
+    ax["u"].set_ylim([-0.04, 0.04])
+    ax["b"].set_ylim([-0.0007, 0.0007])
+
+    ax["du"].set_ylim([-1.3, -0.3])
+    ax["db"].set_ylim([0.005, 0.05])
+
+    ax["Ri"].set_ylabel("Ri$_b =  Δbh/Δu²$")
+    ax["Ri"].set_yscale("log")
+    ax["Ri"].set_yticks([0.25, 0.5, 1, 5, 10])
+    ax["Ri"].grid(True)
+
+    rhs.plot(
+        ax=ax["contrib"],
+        x="longitude",
+        color="k",
+        label="RHS",
+        **kwargs,
+        add_legend=False,
+    )
+    if add_legend:
+        ax["contrib"].legend(ncol=5)
+        dcpy.plots.liney(0, ax=ax["contrib"])
+    ax["contrib"].set_ylabel("Fractional changes")
+    ax["contrib"].set_title("")
+    ax["contrib"].set_ylim([-0.15, 0.1])
+
+    name = ds.attrs["name"]
+    if add_legend:
+        ax["Ri"].set_title(f"latitude = 0, {name} dataset")
+    else:
+        ax["Ri"].set_title(f"latitude = 0")
+
+    f.set_size_inches(8, 10)
+
+    # xr.testing.assert_allclose(ri, rhs)
+
+    return f, ax
