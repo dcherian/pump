74f0c8f0fcf9dbf37e5a71502d1cfb0b727d80bd
dcherian <deepak@cherian.net>
2019-10-17 08:31:17 -0600

    Add more compositing code.

--- pump/composite.py
+++ pump/composite.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy as sp
 import xarray as xr
 
 
 def _get_tiv_extent_single_period(data, debug=False):
-    import scipy as sp
 
     indexes, properties = sp.signal.find_peaks(-data, prominence=0.5)
 
@@ -42,7 +42,7 @@ def get_tiv_extent(data, dim="latitude", debug=False):
     return data.latitude[indexes]
 
 
-def get_latitude_reference(data, debug=False):
+def _get_latitude_reference(data, debug=False):
     reference = get_tiv_extent(data.sel(latitude=slice(-6, 6)), debug=debug)
 
     y = xr.full_like(data, fill_value=np.nan)
@@ -79,9 +79,62 @@ def get_y_reference(theta, periods=None, debug=False):
 
     # plt.figure(); t180.plot.line(hue="period")
 
-    yref = get_latitude_reference(t180, debug=debug)
+    yref = _get_latitude_reference(t180, debug=debug)
     ynew = xr.full_like(theta, fill_value=np.nan)
     for period in np.unique(yref.period.values):
         ynew = xr.where(theta.period == period, yref.sel(period=period), ynew)
 
     return ynew
+
+
+def to_uniform_grid(data, coord, new_coord=np.arange(-4, 4, 0.01)):
+
+    if isinstance(new_coord, np.ndarray):
+        new_coord = xr.DataArray(new_coord, dims=[coord], name=coord)
+
+    def _wrap_interp(x, y, newy):
+        f = sp.interpolate.interp1d(
+            x.squeeze(), y.squeeze(), bounds_error=False, fill_value=np.nan
+        )
+        return f(newy)
+
+    result = xr.apply_ufunc(
+        _wrap_interp,
+        data[coord],
+        data,
+        new_coord,
+        input_core_dims=[["latitude"], ["latitude"], ["yref"]],
+        output_core_dims=[["yref"]],  # order is important
+        exclude_dims=set(["latitude"]),  # since size of dimension is changing
+        vectorize=True,
+        dask="parallelized",
+        output_dtypes=[np.float64],
+    )
+
+    result[coord] = new_coord
+
+    return result
+
+
+def make_compositedata:
+    interped = to_uniform_grid(data, "yref", np.arange(-4, 4, 0.01))
+    phase_grouped = interped.groupby_bins("tiw_phase", np.arange(0, 360, 5))
+    data_vars = data.data_vars
+    composite = {name: xr.Dataset(attrs={"name": name}) for name in data_vars}
+    attr_to_name = {"mean": "avg_full", "std": "dev"}
+
+    for attr in ["mean", "std"]:
+        computed = getattr(phase_grouped, attr)()
+        for name in data_vars:
+            composite[name][attr_to_name[attr]] = computed[name]
+            composite[name]["period"] = np.unique(interped.period)
+            # composite[name] = composite[name].transpose("yref", "tiw_phase_bins", "period")
+
+    for name in data_vars:
+        composite[name]["err"] = composite[name].dev / np.sqrt(
+            len(composite[name].period)
+        )
+        is_significant = np.abs(composite[name]).avg_full >= 1.96 * composite[name].err
+        composite[name]["avg"] = composite[name].avg_full.where(is_significant)
+
+    return composite
