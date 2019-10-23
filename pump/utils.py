eae9906b067b00004675f50c233d0ba8149f148f
dcherian <deepak@cherian.net>
2019-10-23 14:45:56 -0600

    Add lowpass filter

--- pump/utils.py
+++ pump/utils.py
@@ -28,3 +28,56 @@ def read_pop(files):
     file0.close()
 
     return ds
+
+
+def lowpass(obj, coord, freq, cycles_per="s", order=2, use_overlap=True, debug=False):
+    """
+    Lowpass butterworth filter
+
+    """
+    import scipy as sp
+    import dcpy
+
+    def _process_time(time, cycles_per="s"):
+
+        time = time.copy()
+        dt = np.nanmedian(np.diff(time.values) / np.timedelta64(1, cycles_per))
+
+        time = np.cumsum(
+            time.copy().diff(dim=time.dims[0]) / np.timedelta64(1, cycles_per)
+        )
+
+        return dt, time
+
+    if obj[coord].dtype.kind == "M":
+        dx, x = _process_time(obj[coord], cycles_per)
+    else:
+        dx = np.diff(obj[coord][0:2].values)
+
+    b, a = sp.signal.butter(order, freq * dx / (1 / 2), btype="low")
+
+    def _wrapper(data):
+        return sp.signal.filtfilt(b, a, data, method="gust")
+
+    if use_overlap:
+        # 1e-2 fails xarray.testing.assert_allclose, 1e-3 and smaller are fine
+        padlen = dcpy.ts.EstimateImpulseResponseLength(b, a, eps=1e-3)
+        axis = obj.get_axis_num(coord)
+        if debug:
+            print(f"padding with length {padlen}")
+        da = obj.chunk({coord: 40 * padlen}).data
+        result = obj.copy(
+            data=da.map_overlap(_wrapper, boundary="none", depth={axis: padlen + 1})
+        )
+
+    else:
+        result = xr.apply_ufunc(
+            _wrapper,
+            obj,
+            dask="parallelized",
+            input_core_dims=[[coord]],
+            output_core_dims=[[coord]],
+            output_dtypes=[np.float64],
+        )
+
+    return result
