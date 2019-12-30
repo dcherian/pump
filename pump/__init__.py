0478c83b40de76a4332d798487b6ddb7943b396f
Deepak Cherian <dcherian@hobart.cgd.ucar.edu>
2019-12-30 14:02:08 -0700

    refiximports

--- pump/__init__.py
+++ pump/__init__.py
@@ -3,7 +3,7 @@ from .constants import *
 from .obs import *
 from .model.model import model
 
-import cesm
-import composite
-import utils
-import calc
+from . import cesm
+from . import composite
+from . import utils
+from . import calc
