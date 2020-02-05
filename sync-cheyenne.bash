77de47b1b6e76cfbd05d5ecf95f8a0f097e048cc
dcherian <deepak@cherian.net>
2020-02-04 18:44:48 -0700

    Update sync-cheyenne

--- sync-cheyenne.bash
+++ sync-cheyenne.bash
@@ -1,2 +1,12 @@
-rsync -ahvPi --exclude 'glade' --exclude '.git' --exclude 'pump.org' chdata:~/pump/* .
+rsync -ahvPi \
+      --exclude 'sync-cheyenne.bash' \
+      --exclude 'datasets' \
+      --exclude 'glorys' \
+      --exclude '__pycache__' \
+      --exclude '*.zarr' \
+      --exclude 'glade' \
+      --exclude '.git' \
+      --exclude 'pump.org' \
+      chdata:~/pump/* .
+rsync -ahvPi --exclude 'modis' chdata:~/pump/datasets/* datasets/
 rsync -ahvPi ~/pump/images/* ~/pump/hugo/static/ox-hugo/
